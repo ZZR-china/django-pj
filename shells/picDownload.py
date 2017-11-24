d93a7b6f70618caed2238940a2346d6c18b8e12e
ZZR-china <2467254599@qq.com>
2017-11-24 09:56:21 +0800

    a

--- shells/picDownload.py
+++ shells/picDownload.py
@@ -12,15 +12,16 @@ import requests
 
 from mkdir import *
 
-def dowmloadPic(html,keyword):
 
-    pic_url = re.findall('"objURL":"(.*?)",',html,re.S)
+def dowmloadPic(html, keyword):
+
+    pic_url = re.findall('"objURL":"(.*?)",', html, re.S)
     i = 0
     print('找到关键词:'+keyword+'的图片，现在开始下载图片...')
     for each in pic_url:
         print('正在下载第'+str(i+1)+'张图片，图片地址:'+str(each))
         try:
-            pic= requests.get(each, timeout=10)
+            pic = requests.get(each, timeout=10)
         except requests.exceptions.ConnectionError:
             print('【错误】当前图片无法下载')
             continue
@@ -30,7 +31,8 @@ def dowmloadPic(html,keyword):
         imgsDirPath = imgsDirPath + '\\' + keyword + '_' + str(i) + '.jpg'
         print(imgsDirPath)
         print(type(imgsDirPath))
-        #resolve the problem of encode, make sure that chinese name could be store
+        # resolve the problem of encode, make sure that chinese name could be
+        # store
         fp = open(imgsDirPath, 'wb')
         fp.write(pic.content)
         fp.close()
@@ -39,6 +41,7 @@ def dowmloadPic(html,keyword):
 
 if __name__ == '__main__':
     word = input("Input key word: ")
-    url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word='+word+'&ct=201326592&v=flip'
+    url = 'http://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&word=' + \
+        word+'&ct=201326592&v=flip'
     result = requests.get(url)
-    dowmloadPic(result.text,word)
+    dowmloadPic(result.text, word)
